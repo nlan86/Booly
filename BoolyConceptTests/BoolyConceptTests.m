1387f3943a33c1cbd05c105bc2e476857a3e488d
Nur Lan <nurlan86@gmail.com>
2015-10-07 11:11:55 +0300

    Initial commit + test dirs

---
+++ BoolyConceptTests/BoolyConceptTests.m
@@ -0,0 +1,39 @@
+//
+//  BoolyConceptTests.m
+//  BoolyConceptTests
+//
+//  Created by NL on 10/2/15.
+//  Copyright © 2015 Nur Lan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BoolyConceptTests : XCTestCase
+
+@end
+
+@implementation BoolyConceptTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
